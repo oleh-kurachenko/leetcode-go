1588cd2d8e9c6410fb9497db5565cfa541ca1ee2
Oleh Kurachenko <oleh.kurachenko@gmail.com>
2026-03-09 21:12:42 +0200

    add problem 94

---
+++ src/00/94.go
@@ -0,0 +1,16 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+
+	return append(append(inorderTraversal(root.Left), root.Val),
+		inorderTraversal(root.Right)...)
+}
